4c8c9092dbaa88a9d7a56c83dd36685279a4ddef
AmbroiseRENE <ambroise.rene@gmail.com>
2019-11-13 12:30:54 +0100

    Modification de la classe Employee

--- Employee.java
+++ Employee.java
@@ -1,16 +1,51 @@
+package com.fr.adaming.entity;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+import com.fr.adaming.enumeration.Fonction;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
-public class Employee {
+@Getter	@Setter @AllArgsConstructor @NoArgsConstructor @ToString
+public class Employee extends User{
 
-	@id
-	private int id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
 	
 	private String nom;
 	
 	private String prenom;
-	
-	@column(unique ="true")
+	@Email
 	private String email;
-	
+
 	private String pwd;
+	
+	private double salaire;
+	
+	@Enumerated(EnumType.ORDINAL)
+	private Fonction fonction;
+
+	private LocalDate dateRecrutement;	
+
+	@ManyToMany(mappedBy = "employees")
+	private Set<Projet> projetsE;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_Societe")
+	private Societe societeE;
+	
 }
