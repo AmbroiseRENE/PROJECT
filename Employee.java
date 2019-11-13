5c64d8473d4864ecdd4210ff7b03ac3e66c18d5b
AmbroiseRENE <ambroise.rene@gmail.com>
2019-11-13 15:31:51 +0100

    ajout des imports sur la classe Employee

--- Employee.java
+++ Employee.java
@@ -2,6 +2,22 @@
 // pinaise moi j'avais deja mis un commentaire ici
 
 // cette classe c'est vraiment trop de la balle
+package com.fr.adaming.entity;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.validation.constraints.Email;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter	@Setter @AllArgsConstructor @NoArgsConstructor @ToString
